c3d97928bdb49014082a398dd1ce6cadc5ad34b9
Mauro Zegarra <mauro.zegarra@gmail.com>
2020-08-10 10:49:34 -0500

    Release starter code

--- app/src/main/java/com/maurozegarra/example/mvvmwithdatabinding/MainActivity.kt
+++ app/src/main/java/com/maurozegarra/example/mvvmwithdatabinding/MainActivity.kt
@@ -2,10 +2,30 @@ package com.maurozegarra.example.mvvmwithdatabinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import com.maurozegarra.example.mvvmwithdatabinding.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            button.setOnClickListener {
+                lifecycleScope.launch {
+                    progress.visibility = View.VISIBLE
+                    message.text = withContext(Dispatchers.IO) {
+                        Thread.sleep(2_000)
+                        if (user.text.isNotEmpty() && pass.text.isNotEmpty()) "Success" else "Failure"
+                    }
+                    progress.visibility = View.GONE
+                }
+            }
+        }
     }
 }
