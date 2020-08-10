22f16c6d852b773d4f04f7a726fe2ab76a749e24
Mauro Zegarra <mauro.zegarra@gmail.com>
2020-08-10 17:57:58 -0500

    Add Data Binding to MVVM

--- app/src/main/java/com/maurozegarra/example/mvvmwithdatabinding/MainActivity.kt
+++ app/src/main/java/com/maurozegarra/example/mvvmwithdatabinding/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.maurozegarra.example.mvvmwithdatabinding
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
 import com.maurozegarra.example.mvvmwithdatabinding.databinding.ActivityMainBinding
@@ -18,19 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         viewModel = ViewModelProvider(this).get()
-
-        viewModel.progressVisibility.observe(this, Observer {
-            binding.progress.visibility = if (it) View.VISIBLE else View.GONE
-        })
-
-        viewModel.message.observe(this, Observer {
-            binding.message.text = it
-        })
-
-        with(binding) {
-            button.setOnClickListener {
-                viewModel.onButtonClicked(user.text.toString(), pass.text.toString())
-            }
-        }
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
     }
 }
